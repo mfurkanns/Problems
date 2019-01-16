a26257a42461d06cf4b3a46b5778cdd568dd2f8d
Furkan <38671021+trmfurkan@users.noreply.github.com>
2019-01-17 01:08:59 +0300

    Update Problem2.c

--- Problem2.c
+++ Problem2.c
@@ -5,31 +5,30 @@ WHAT IS THE PROBABILITY OF REACHING TO THE 1. STEP OF THE ROAD ? */
 #include <stdio.h>
 #include <time.h>
 
-
-int main(){
-	
+int main(){	
+		
 	srand(time(NULL));
 	
 	int i,j,current_position=0,n=1,coin;
 	double coming=0;
 	
-	for(i=0;i<1000;i++){  							// lets try 1000 times
-		for(j=0;j<10000;j++){						// we can go back in our every movement, it means n = - infinite
+	for(i=0;i<1000;i++){  					// lets try 1000 times
+		for(j=0;j<10000;j++){				// we can go back in our every movement, it means n = - infinite
 			
-			coin=rand()%2;							// throw the coin
+			coin=rand()%2;				// throw the coin
 			if(coin==0){
-				current_position++;					// if it comes heads go to the next step
-				if(current_position==n){			// did i reach ?
+				current_position++;		// if it comes heads go to the next step
+				if(current_position==n){	// did i reach ?
 					coming++;							
-					current_position=0;				// if i have reached then position have to be zero to reach again to n=1. step
+					current_position=0;	// if i have reached then position have to be zero to reach again to n=1. step
 					break;
 				}
 			}
 			else{
-				current_position--;					// we don't need to check arrival because we cannot reach with going previous step
+				current_position--;   // we don't need to check arrival because we cannot reach with going previous step
 			}			
 		}
-		if(current_position!=n){					// if we didn't reach then position have to be zero to reach again to n=1. step
+		if(current_position!=n){	// if we didn't reach then position have to be zero to reach again to n=1. step
 			current_position=0;
 		}
 	}
